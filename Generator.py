67d0abf9331b1e031e763c892637d507295a6fc3
Abhishek kanaujia <Kanaujia.abhishek20@gmail.com>
2020-02-19 04:38:50 +0530

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 import requests
+# Python program to print 
+# red text with green background 
+
+from colorama import Fore, Back, Style 
+
+print(Style.RESET_ALL) 
+
 
 # define an empty list
 list = []
@@ -16,11 +23,16 @@ with open('file.txt', 'r') as filehandle:
 
 print("URL", "Status_code",sep="------------>")
 try: 
-
 	for i in list:
-	 resp = requests.get(i,timeout=10)
-	 print(i ,resp.status_code, sep="------------>")
-	 resp.raise_for_status()                 # Raise error in case of failure 
+	 #resp = requests.post(i, headers=headers, timeout=10)
+	 resp = requests.post(i,timeout=10,verify=True) 
+	 print(i , Fore.BLUE, resp.status_code, sep="------------>")
+	 with open("file.txt", "r") as f:
+    	  lines = f.readlines() 
+    	  lines.remove(i+"\n")
+    	  with open("file.txt", "w") as new_f:
+        	    for line in lines:             new_f.write(line)
+	
 except requests.exceptions.HTTPError as httpErr: 
     print ("Http Error:",httpErr) 
 except requests.exceptions.ConnectionError as connErr: 
@@ -29,3 +41,6 @@ except requests.exceptions.Timeout as timeOutErr:
     print ("Timeout Error:",timeOutErr) 
 except requests.exceptions.RequestException as reqErr: 
     print ("Something Else:",reqErr) 
+except requests.exceptions.Connecting as ConErr: 
+    print ("Something Else:",ConErr) 
+    
