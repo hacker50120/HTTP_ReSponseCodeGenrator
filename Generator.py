303cdc3e1a0560333fb92967088a4ba273a2881b
Abhishek kanaujia <Kanaujia.abhishek20@gmail.com>
2020-02-19 17:16:11 +0530

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-
+import urllib3
+urllib3.disable_warnings()
 import requests
-# Python program to print 
-# red text with green background 
-
 from colorama import Fore, Back, Style 
-
 print(Style.RESET_ALL) 
-
 
 # define an empty list
 list = []
 
+var =0
 # open file and read the content in a list
 with open('file.txt', 'r') as filehandle:
     for line in filehandle:
@@ -22,25 +19,25 @@ with open('file.txt', 'r') as filehandle:
         list.append(currentPlace)
 
 print("URL", "Status_code",sep="------------>")
-try: 
-	for i in list:
-	 #resp = requests.post(i, headers=headers, timeout=10)
-	 resp = requests.post(i,timeout=10,verify=True) 
-	 print(i , Fore.BLUE, resp.status_code, sep="------------>")
-	 with open("file.txt", "r") as f:
-    	  lines = f.readlines() 
-    	  lines.remove(i+"\n")
-    	  with open("file.txt", "w") as new_f:
-        	    for line in lines:             new_f.write(line)
-	
-except requests.exceptions.HTTPError as httpErr: 
-    print ("Http Error:",httpErr) 
-except requests.exceptions.ConnectionError as connErr: 
-    print ("Error Connecting:",connErr) 
-except requests.exceptions.Timeout as timeOutErr: 
-    print ("Timeout Error:",timeOutErr) 
-except requests.exceptions.RequestException as reqErr: 
-    print ("Something Else:",reqErr) 
-except requests.exceptions.Connecting as ConErr: 
-    print ("Something Else:",ConErr) 
-    
+while(1): 	
+	try: 
+		
+		for i in list:
+		 #resp = requests.post(i, headers=headers, timeout=10)
+		 resp = requests.get(i,timeout=30,verify=False) 
+		 print(Fore.GREEN,i , resp.status_code, sep="------------>")
+		 with open("file.txt", "r") as f:
+	    	  lines = f.readlines()
+	    	  lines.remove(i+"\n")
+	    	  with open("file.txt", "w") as new_f:
+	        	    for line in lines:             
+	        	    			new_f.write(line) 
+	        	    			 
+	        	   		
+	except :
+	    print("error Occure, It may Be some reason Please Retry again...!!")
+	    with open("file.txt", "r") as f:
+           	 lines = f.readlines() 
+           	 lines.remove(list[0]+"\n")
+           	 with open("file.txt", "w") as new_f:
+            	  for line in lines:             new_f.write(line)
