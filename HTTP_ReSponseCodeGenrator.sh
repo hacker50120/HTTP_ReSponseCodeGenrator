10acc473588bd0e12819d6c34b05b683c23abc56
Abhishek kanaujia <Kanaujia.abhishek20@gmail.com>
2020-02-19 17:23:52 +0530

    Update HTTP_ReSponseCodeGenrator.sh

--- HTTP_ReSponseCodeGenrator.sh
+++ HTTP_ReSponseCodeGenrator.sh
@@ -8,8 +8,5 @@ sudo apt-get install ca-certificates
 sudo apt-get install ntpdate
 sudo ntpdate -u ntp.ubuntu.com
 pip install --upgrade certifi
-
-mkdir HTTP_ReSponseCodeGenrator
-cd HTTP_ReSponseCodeGenrator
 touch file.txt
 gedit file.txt
