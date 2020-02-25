af39a36e48d6854eb3bb0b73d091e82ac2f0ca36
Abhishek kanaujia <Kanaujia.abhishek20@gmail.com>
2020-02-25 21:34:10 +0530

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import urllib3
+import sys
 urllib3.disable_warnings()
 import requests
 from colorama import Fore, Back, Style
@@ -7,39 +8,49 @@ print(Style.RESET_ALL)
 
 # define an empty list
 list = []
-
+var =1
 # open file and read the content in a list
-with open('file.txt', 'r') as filehandle:
-    var = 0
-    for line in filehandle:
-        # remove linebreak which is the last character of the string
-        currentPlace = line[:-1]
-        var += 1
-        # add item to the list
-        list.append(currentPlace)
-while (var > 0):
-    print("URL", "Status_code", sep="------------>")
-    while (1):
-        try:
+def reader():
+    with open('file.txt', 'r') as filehandle:
+        var = 0
+        for line in filehandle:
+            # remove linebreak which is the last character of the string
+            currentPlace = line[:-1]
+            var += 1
+            # add item to the list
+            list.append(currentPlace)
 
-            for i in list:
-                # resp = requests.post(i, headers=headers, timeout=10)
-                resp = requests.get(i, timeout=(6, 30), verify=False)
-                print(i, resp.status_code, sep="------------>")
+def scanner():
+    for i in list:
+        # resp = requests.post(i, headers=headers, timeout=10)
+        resp = requests.get(i, timeout=(3.2, 30), verify=False)
+        print(i, resp.status_code, sep="------------>")
+        with open("file.txt", "r") as f:
+            lines = f.readlines()
+            lines.remove(i + "\n")
+            with open("file.txt", "w") as new_f:
+                for line in lines:
+                    new_f.write(line)
+reader()
+
+while (var > 0):
+    try:
+        print("URL", "Status_code", sep="------------>")
+        while (1):
+            try:
+                scanner()
+
+            except:
+                reader()
+                print(list[0]+"<<<----error Occure, Due To Connection Time Out...!!---->")
                 with open("file.txt", "r") as f:
                     lines = f.readlines()
-                    lines.remove(i + "\n")
+                    lines.remove(list[0] + "\n")
                     with open("file.txt", "w") as new_f:
                         for line in lines:
-                            new_f.write(line)
+                                new_f.write(line)
+                        scanner()
 
-
-
-        except:
-            print("error Occure, It may Be some reason Please Retry again...!!")
-            with open("file.txt", "r") as f:
-                lines = f.readlines()
-                lines.remove(list[0] + "\n")
-                with open("file.txt", "w") as new_f:
-                    for line in lines:             new_f.write(line)
-
+    except:
+        print("DONE!! \nPlease Enter the Url's In file.txt")
+        sys.exit(0)
